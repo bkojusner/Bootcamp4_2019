c662eb1122824696d5f18c86043f12d92283dd0b
Blas Kojusner <bkojusner@ufl.edu>
2019-10-28 10:52:51 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import HomepageImage from './components/HomepageImage'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <HomepageImage />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Niekaab Desal
         </p>
         <a
           className="App-link"
